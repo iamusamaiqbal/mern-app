8d5af92e899624a582ae85167c4825a76a3917fe
Usama <iamusamaiqbal@gmail.com>
2022-11-14 19:47:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {Table, Button} from 'react-bootstrap';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const[userList,setUserList] = useState([]);
+
+  const getData = () => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(response => setUserList(response.data))
+      .catch(err => console.log(err));
+  }
+
+  // useEffect(() => {
+  //    getData();
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Company</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        {userList.map((user) => (
+          <tr>
+            <td>{user.id }</td>
+            <td>{user.name }</td>
+            <td>{user.email }</td>
+            <td>{user.address.suite }, {user.address.street },{user.address.city }</td>
+            <td>{user.id }</td>
+            
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+    <Button onClick={getData}> Click to load data</Button>
+    </>
+
   );
 }
 
